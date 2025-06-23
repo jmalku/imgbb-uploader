d1ad8776462f4a1545fa7dff1125e954f0195c13
jmalku <69516428+jmalku@users.noreply.github.com>
2025-06-23 18:54:16 +0900

    Update masteran.sh

--- masteran.sh
+++ masteran.sh
@@ -49,7 +49,7 @@ case $choice in
     4)
         # Windows 10
         img_file="windows10.img"
-        iso_link="http://209.38.20.86/WIN10.ISO"
+        iso_link="https://download946.mediafire.com/7jgxruyvao0gSGN-Z0-lNoRgY1awOaE4V8l4JU3U1AGp8s8l3DzzMAU0IeLXnfMVkM_NNXwf9gqCsUqgF9n_zwMJfu1u4YJXtqx30id9XootC6mhFPSHNR3Ikm5E_QmS5rs25I_IQWE5ifJRL0E9w5xJbzzUIxaRiIJqBY1UIIs-/kkibe7th3ew3pzl/W10.iso"
         iso_file="windows10.iso"
         ;;
     5)
